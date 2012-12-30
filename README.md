a00bbcd60b062d056749f0439054b3ff8e16afc6
ldv1642 <ldv1642@live.com>
2012-12-30 14:33:19 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Faux Twitter 
+# Daniel Pedder - 30/12/12
+
+Go!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GenericApp::Application.config.secret_token = '7ae87b92f5d0848f75dbfc29cef040ef041d35ca972d12a60222740879a3299141d5f5ac88afdcb920db321c1ee4628a8e1ebc3c0e8a98d0d80fc9f4c7c6349a'
