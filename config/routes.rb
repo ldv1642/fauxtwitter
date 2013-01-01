2b1e2da08154833d951110b35e7a973d4c8a4fba
ldv1642 <ldv1642@live.com>
2013-01-01 21:29:35 +0000

    Added pg gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 GenericApp::Application.routes.draw do
-  match '/signup', to: 'users#new'
+  get '/signup', to: 'users#new'
 
-  match '/cheat', to: 'misc#cheatsheet'
+  get '/cheat', to: 'misc#cheatsheet'
   root to: 'static_pages#home'
-  match '/help', to: 'static_pages#help'
-  match '/contact', to: 'static_pages#contact'
-  match '/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
+  get '/contact', to: 'static_pages#contact'
+  get '/about', to: 'static_pages#about'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
