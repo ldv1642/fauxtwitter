83f6210b7fc4c0d75927a21901b35500e922f9bf
ldv1642 <ldv1642@live.com>
2013-01-02 18:51:31 +0000

    User model created

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,16 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name
 
   validates :name, presence: true, length: { maximum: 50 }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  
+  validates :email, presence: true, 
+  format: { with: VALID_EMAIL_REGEX }, 
+  uniqueness: { case_sensitive: false }
+
+  #sorts out case sensitive issues with some db adapters when creating indicies
+  before_save do |user|
+  	user.email = email.downcase
+  end
+
 end
  
