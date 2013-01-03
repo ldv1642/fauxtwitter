4ab598176bf952ee207114d2b541530fce18a329
ldv1642 <ldv1642@live.com>
2013-01-03 15:54:14 +0000

    Remove ANSI colour codes

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module GenericApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    #disable colour escape code
+    config.colorize_logging = false
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
